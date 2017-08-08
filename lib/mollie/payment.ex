23bb092dea4d00f169f6ffb65fce6fb2637b88c1
Tom Pesman <tom@9to5.software>
2017-08-08 16:26:05 +0200

    subscriptionId added to payment

--- lib/mollie/payment.ex
+++ lib/mollie/payment.ex
@@ -3,15 +3,16 @@ defmodule Mollie.Payment do
   alias Mollie.Error
 
   defstruct [:id, :amount, :description, :redirectUrl, :webhookUrl, :method, :metadata, :locale, :recurringType,
-             :customerId, :mandateId, :issuer, :profileId, :testmode, :applicationFee, :status, :links, :mode,
-             :createDatetime, :paidDatetime, :cancelledDatetime, :expiredDatetime, :expiryPeriod, :amountRefunded,
-             :amountRemaining, :countryCode, :settlementId, :details]
+             :customerId, :mandateId, :subscriptionId, :issuer, :profileId, :testmode, :applicationFee, :status, :links,
+             :mode, :createDatetime, :paidDatetime, :cancelledDatetime, :expiredDatetime, :expiryPeriod,
+             :amountRefunded, :amountRemaining, :countryCode, :settlementId, :details]
 
   @type t :: %__MODULE__{id: binary, amount: binary, description: binary, redirectUrl: binary, webhookUrl: binary,
-                         method: binary, metadata: binary, locale: binary, recurringType: binary, customerId: binary,
-                         mandateId: binary, issuer: binary, profileId: binary, testmode: binary, applicationFee: binary,
-                         status: binary, links: %{Atom => binary}, mode: binary, createDatetime: binary,
-                         paidDatetime: binary, cancelledDatetime: binary, expiredDatetime: binary, expiryPeriod: binary,
+                         method: binary, metadata: %{Atom => binary}, locale: binary, recurringType: binary,
+                         customerId: binary, mandateId: binary, subscriptionId: binary, issuer: binary,
+                         profileId: binary, testmode: binary, applicationFee: binary, status: binary,
+                         links: %{Atom => binary}, mode: binary, createDatetime: binary, paidDatetime: binary,
+                         cancelledDatetime: binary, expiredDatetime: binary, expiryPeriod: binary,
                          amountRefunded: binary, amountRemaining: binary, countryCode: binary, settlementId: binary,
                          details: binary}
 
